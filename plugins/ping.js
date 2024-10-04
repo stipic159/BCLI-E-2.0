8f52fe43c6cf6577f9c35899dad42975130a1a0e
stipic159 <stpogood@gmail.com>
2024-10-04 20:42:09 +0300

    database + first command

---
+++ plugins/ping.js
@@ -0,0 +1,13 @@
+module.exports = {
+    pattern: 'ping',
+    alias: ['пинг'],
+    desc: 'Проверка скорости работы бота',
+    category: ['🏠 Базовые команды'],
+    async execute(Void, citel) {
+        let initial = new Date().getTime();
+        const { key } = await Void.sendMessage(citel.chat, { text: '_*Пинг*_' }, { quoted: citel });
+        let final = new Date().getTime();
+        const ping = final - initial;
+        await Void.sendMessage(citel.chat, { text: `Время отклика: ${ping} мс`, edit: key })
+    }
+};
